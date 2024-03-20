d162bc692a2d506272abb234602b9e9eb4258725
Victoriia-Ripka <vikaripka@gmail.com>
2024-03-20 15:15:25 +0100

    update

--- lab3/task.py
+++ lab3/task.py
@@ -1,9 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-def euler_method(f, y0, x0, xn, h):
+def euler_method(x0, xn, y0, h):
     n = int((xn - x0) / h)
-    x_values = np.linspace(x0, xn, n+1)
+    x_values = np.array([1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4])
     y_values = np.zeros(n+1)
     y_values[0] = y0
 
@@ -12,42 +12,44 @@ def euler_method(f, y0, x0, xn, h):
 
     return x_values, y_values
 
-def euler_cauchy_method(f, y0, x0, xn, h):
+def euler_cauchy_method(x0, xn, y0, h):
     n = int((xn - x0) / h)
-    x_values = np.linspace(x0, xn, n+1)
+    x_values = np.array([1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4])
     y_values = np.zeros(n+1)
     y_values[0] = y0
 
-    for i in range(1, n+1):
-        y_half = y_values[i-1] + 0.5 * h * f(x_values[i-1], y_values[i-1])
-        y_values[i] = y_values[i-1] + h * f(x_values[i-1] + 0.5 * h, y_half)
+    y_values[1] = y_values[0] + h * f(x_values[0], y_values[0])
+
+    for i in range(2, n+1):
+        spec_y = y_values[i-1] + h * f(x_values[i-1], y_values[i-1])
+        y_values[i] = y_values[i-1] + 0.5 * h * ( f(x_values[i-1], y_values[i-1]) + f(x_values[i], spec_y))
 
     return x_values, y_values
 
-def improved_euler_method(f, y0, x0, xn, h):
+def improved_euler_method(x0, xn, y0, h):
     n = int((xn - x0) / h)
-    x_values = np.linspace(x0, xn, n+1)
+    x_values = np.array([1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4])
     y_values = np.zeros(n+1)
     y_values[0] = y0
 
     for i in range(1, n+1):
-        y_predictor = y_values[i-1] + h * f(x_values[i-1], y_values[i-1])
-        y_values[i] = y_values[i-1] + 0.5 * h * (f(x_values[i-1], y_values[i-1]) + f(x_values[i], y_predictor))
+        spec_y = y_values[i-1] + 0.5 * h * f(x_values[i-1], y_values[i-1])
+        y_values[i] = y_values[i-1] + h * f(x_values[i-1] + h * 0.5, spec_y)
 
     return x_values, y_values
 
-def runge_kutta_4(f, y0, x0, xn, h):
+def runge_kutta_4(x0, xn, y0, h):
     n = int((xn - x0) / h)
-    x_values = np.linspace(x0, xn, n+1)
+    x_values = np.array([1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4])
     y_values = np.zeros(n+1)
     y_values[0] = y0
 
     for i in range(1, n+1):
-        k1 = h * f(x_values[i-1], y_values[i-1])
-        k2 = h * f(x_values[i-1] + 0.5 * h, y_values[i-1] + 0.5 * k1)
-        k3 = h * f(x_values[i-1] + 0.5 * h, y_values[i-1] + 0.5 * k2)
-        k4 = h * f(x_values[i-1] + h, y_values[i-1] + k3)
-        y_values[i] = y_values[i-1] + (k1 + 2*k2 + 2*k3 + k4) / 6
+        k0 = f(x_values[i-1], y_values[i-1])
+        k1 = f(x_values[i-1] + 0.5 * h, y_values[i-1] + 0.5 * h * k0)
+        k2 = f(x_values[i-1] + 0.5 * h, y_values[i-1] + 0.5 * h * k1)
+        k3 = f(x_values[i-1] + h, y_values[i-1] + h * k2)
+        y_values[i] = y_values[i-1] + (k0 + 2*k1 + 2*k2 + k3) * h / 6
 
     return x_values, y_values
 
@@ -55,30 +57,30 @@ def f(x, y):
     return y + np.sin(x/2.8)
 
 # Задані параметри
-x0, xn = 1.4, 2.3999
+x0, xn = 1.4, 2.4
 y0 = 2.2
 h = 0.1
 
 # Виклик методів
-x_euler, y_euler = euler_method(f, y0, x0, xn, h)
-x_euler_cauchy, y_euler_cauchy = euler_cauchy_method(f, y0, x0, xn, h)
-x_improved_euler, y_improved_euler = improved_euler_method(f, y0, x0, xn, h)
-x_rk4, y_rk4 = runge_kutta_4(f, y0, x0, xn, h)
+x_euler, y_euler = euler_method(x0, xn, y0, h)
+x_euler_cauchy, y_euler_cauchy = euler_cauchy_method(x0, xn, y0, h)
+x_improved_euler, y_improved_euler = improved_euler_method(x0, xn, y0, h)
+x_rk4, y_rk4 = runge_kutta_4(x0, xn, y0, h)
 
 # Точний розв'язок для порівняння
-x_exact = np.linspace(x0, xn, 10)
-y_exact = 2.2 * np.exp(x_exact - 1.4) - np.sin(x_exact / 2.8) + np.sin(1.4 / 2.8)
+x_exact = np.array([1.4, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4])
+y_exact = np.array([2.2, 2.482407, 2.799851, 3.152735, 3.545817, 3.983248, 4.469607, 5.009956, 5.609882, 6.275559, 7.013805])
 print(x_exact, '\n', y_exact, '\n\n')
 
 
 if len(y_exact) == len(y_euler):
     print('euler:          ', y_euler)
-    print('error delta:    ', abs(y_exact-y_euler))
+    print('error delta:    ', abs(y_exact - y_euler))
 else:
     print('Розмірності не співпадають. Неможливо порівняти.')
 
 print('euler_cauchy:   ', y_euler_cauchy)
-print('error delta:    ', abs(y_exact-y_euler_cauchy))
+print('error delta:    ', abs(y_exact - y_euler_cauchy))
 print('improved_euler: ', y_improved_euler)
 print('error delta:    ', abs(y_exact-y_improved_euler))
 print('runge_kutta:    ', y_rk4)
